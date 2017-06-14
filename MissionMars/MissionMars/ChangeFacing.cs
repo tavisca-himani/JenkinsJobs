51873bafd4cd0c37faf07b51254360b05a5b4343
tavisca-himani <hverma@tavisca.com>
2017-06-14 10:51:13 +0530

    MissionMars

---
+++ MissionMars/MissionMars/ChangeFacing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionMars
+{
+    public class ChangeFacing :MarsRover
+    {
+        public static void changeDirection(ref char newFacing,ref char facingOfRover)
+        {
+            if (newFacing == 'L')
+            {
+                if (facingOfRover == 'N')
+                    facingOfRover = 'W';
+
+                else if (facingOfRover == 'S')
+                    facingOfRover = 'E';
+
+                else if (facingOfRover == 'E')
+                    facingOfRover = 'N';
+
+                else if (facingOfRover == 'W')
+                    facingOfRover = 'S';
+            }
+            else
+            {
+                if (facingOfRover == 'N')
+                    facingOfRover = 'E';
+
+                else if (facingOfRover == 'S')
+                    facingOfRover = 'W';
+
+                else if (facingOfRover == 'E')
+                    facingOfRover = 'S';
+
+                else if (facingOfRover == 'W')
+                    facingOfRover = 'N';
+            }
+        }
+    }
+}
---
+++ MissionMars/MissionMars/ChangePosition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionMars
+{
+    public class ChangePosition : MarsRover
+    {
+        public  static void rover_Moving(ref char currentInstruction, ref char facingOfRover)
+        {
+            if (CheckMovementOfRover.checkIfRoverCanMove(ref currentInstruction, ref facingOfRover))
+            {
+                if ((facingOfRover == 'N' && currentInstruction == 'F') || (facingOfRover == 'S' && currentInstruction == 'B'))
+                    currentPositionYOfRoverOnMars++;
+
+                else if ((facingOfRover == 'E' && currentInstruction == 'F') || (facingOfRover == 'W' && currentInstruction == 'B'))
+                    currentPositionXOfRoverOnMars++;
+
+                else if ((facingOfRover == 'S' && currentInstruction == 'F') || (facingOfRover == 'N' && currentInstruction == 'B'))
+                    currentPositionYOfRoverOnMars--;
+
+                else if ((facingOfRover == 'W' && currentInstruction == 'F') || (facingOfRover == 'E' && currentInstruction == 'B'))
+                    currentPositionXOfRoverOnMars--;
+            }
+            
+            
+        }
+        
+
+    }
+}
