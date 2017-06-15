c965d0c96713334eaf3b483e7f33a74c5c4ca99e
tavisca-himani <hverma@tavisca.com>
2017-06-15 20:01:07 +0530

    NotificationApplication

--- NotificationApplication/NotificationApplication/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace NotificationApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- NotificationApplication/NotificationApplication/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace NotificationApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
